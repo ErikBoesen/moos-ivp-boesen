7c9460a2b5751e77484b9589f1a20488c48908fd
ErikBoesen <me@erikboesen.com>
2018-06-19 10:35:40 -0400

    Add initial pOdometry

---
+++ src/pOdometry/Odometry.h
@@ -0,0 +1,33 @@
+/************************************************************/
+/*    NAME: Erik Boesen                                              */
+/*    ORGN: MIT                                             */
+/*    FILE: Odometry.h                                          */
+/*    DATE:                                                 */
+/************************************************************/
+
+#ifndef Odometry_HEADER
+#define Odometry_HEADER
+
+#include "MOOS/libMOOS/MOOSLib.h"
+
+class Odometry : public CMOOSApp
+{
+ public:
+   Odometry();
+   ~Odometry();
+
+ protected: // Standard MOOSApp functions to overload  
+   bool OnNewMail(MOOSMSG_LIST &NewMail);
+   bool Iterate();
+   bool OnConnectToServer();
+   bool OnStartUp();
+
+ protected:
+   void RegisterVariables();
+
+ private: // Configuration variables
+
+ private: // State variables
+};
+
+#endif 
